1474c4d8e1e29a1fb1364958fdba90c5cd099743
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-26 16:37:25 +0800

    add `--version` and `--help` - fixes #2

--- dark-mode/main.m
+++ dark-mode/main.m
@@ -3,7 +3,7 @@
 //  dark-mode
 //
 //  Created by Sindre Sorhus on 18/10/14.
-//  Copyright (c) 2014 Sindre Sorhus. All rights reserved.
+//  Copyright (c) 2015 Sindre Sorhus. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -14,13 +14,25 @@ int main() {
 		NSString *mode = CFBridgingRelease(CFPreferencesCopyValue((CFStringRef)@"AppleInterfaceStyle", kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost));
 		NSString *newMode = [mode isEqualToString: @"Dark"] ? @"Light" : @"Dark";
 
-		if (args.count > 1 && [args[1] isEqualToString: @"--mode"]) {
-			if (args.count == 2) {
-				printf("%s\n", [mode UTF8String]);
+		if (args.count > 1) {
+			if ([args[1] isEqualToString: @"--version"]) {
+				puts("1.0.1");
 				return 0;
 			}
 
-			newMode = args[2];
+			if ([args[1] isEqualToString: @"--help"]) {
+				puts("\n  Usage:\n    dark-mode [--mode]\n\n  Options:\n    --mode  Get or set the mode: Dark|Light\n\n  Created by Sindre Sorhus");
+				return 0;
+			}
+
+			if ([args[1] isEqualToString: @"--mode"]) {
+				if (args.count == 2) {
+					printf("%s\n", [mode UTF8String]);
+					return 0;
+				}
+
+				newMode = args[2];
+			}
 		}
 
 		CFPreferencesSetValue((CFStringRef)@"AppleInterfaceStyle", (__bridge CFPropertyListRef)(newMode), kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost);
